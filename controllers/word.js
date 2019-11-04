800f5201a2c6b88858cd0d1a91ffa5eb2b45b5c4
Felipe Pena <felipe.pena@hubster.com.br>
2019-11-03 23:06:19 -0300

    ADD get words for game

--- controllers/word.js
+++ controllers/word.js
@@ -19,7 +19,75 @@ module.exports.list = async (server, req, res) => {
 };
 
 module.exports.listForGame = async (server, req, res) => {
-  let words = await server.models.dao.index.models.Word.find({});
+  let chosenWords = [];
+
+  try {
+    const category = req.query.category;
+
+    const foundCategory = await server.models.dao.index.models.Category.findOne({
+      name: category,
+    });
+
+    const words = await server.models.dao.index.models.Word.find({
+      category: foundCategory._id,
+    });
+
+    if (words && words.length > 0) {
+      const wordsUpToTwoSyllables = words.filter((word) => {
+        const syllables = word.separatedBySyllables.split('-');
+
+        for (let i = 0; i < syllables.length; i++) {
+          if (syllables[i].length > 3) {
+            return false;
+          }
+        }
+
+        return syllables.length === 2;
+      });
+
+      const wordsUpToThreeSyllables = words.filter((word) => {
+        const syllables = word.separatedBySyllables.split('-');
+
+        for (let i = 0; i < syllables.length; i++) {
+          if (syllables[i].length > 3) {
+            return false;
+          }
+        }
+
+        return syllables.length === 3;
+      });
+
+      const twoSyllablesWord = wordsUpToTwoSyllables[Math.floor(
+          Math.random() * wordsUpToTwoSyllables.length)];
+
+      const twoSyllablesWordArray =
+          twoSyllablesWord.separatedBySyllables.split('-');
+
+      const indexToRemove =
+          Math.floor(Math.random() * wordsUpToThreeSyllables.length);
+
+      const threeSyllablesWord1 = wordsUpToThreeSyllables[indexToRemove];
+
+      wordsUpToThreeSyllables.splice(indexToRemove, 1);
+
+      const threeSyllablesWordArray1 =
+          threeSyllablesWord1.separatedBySyllables.split('-');
+
+      const threeSyllablesWord2 = wordsUpToThreeSyllables[Math.floor(
+          Math.random() * wordsUpToThreeSyllables.length)];
+
+      const threeSyllablesWordArray2 =
+            threeSyllablesWord2.separatedBySyllables.split('-');
+
+      chosenWords = [...twoSyllablesWordArray,
+        ...threeSyllablesWordArray1,
+        ...threeSyllablesWordArray2];
+    }
+  } catch (e) {
+
+  }
+
+  res.status(200).send(chosenWords);
 };
 
 module.exports.createForm = (server, req, res) => {
